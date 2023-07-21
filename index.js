1319a7020c911ef1fa8fc333c19e2a753b7ad123
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-07-21 17:45:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+const express = require('express');
+const app = express();
+const http = require('http');
+// creating http server and passing express as request handler
+const server = http.createServer(app); //access a request handler
+const path = require('path');
+const socketio = require('socket.io');
+const io = socketio(server);
+
+app.use('/' , express.static(path.join(__dirname , 'public'))); 
+
+
+
+const users = {};
+
+
+io.on('connection' , (socket)=>{
+    // console.log(socket.id)
+    console.log(`connection established with ${socket.id}`);
+
+    socket.on('send-msg' , (data)=>{ //listen to some event
+        // console.log(data);
+        io.emit('received-msg' , {
+            msg: data.msg,
+            username: users[socket.id]
+        })
+    }) 
+
+    socket.on('login' , (data)=>{
+        // console.log(data,'sam');
+        users[socket.id] = data.username; //mapping key/id's with username
+
+    })
+
+})
+
+
+
+// static files ko use krne ke liye
+app.use('/' , express.static(path.join(__dirname , 'public')));
+
+
+
+const port = process.env.PORT || 3000;
+server.listen(port , ()=>{
+    console.log(`server connected at port ${port}`)
+})
---
+++ public/script.js
@@ -0,0 +1,39 @@
+
+const socket = io();
+
+$('#chat-box').hide();
+
+$('#send-btn').on('click' ,()=>{
+    // console.log('clicked')
+    const msgText = $('#inp').val();
+
+    socket.emit('send-msg' , {
+        msg:msgText
+    })
+
+    $('#inp').val("")
+});
+
+
+socket.on('received-msg' , (data)=>{
+    // console.log(data)
+    $('#chat').append(`<li class="border p-2 ms-0 mb-2 rounded-pill"><span class="fw-bold"> ${data.username} </span>-> ${data.msg} <span></span> </li>`)
+})
+
+
+
+$('#login-btn').on('click' ,()=>{
+    const username = $('#username').val();
+    // console.log(username);
+
+    socket.emit('login',{
+        username : username
+    })
+
+    $('#login').hide();
+    $('#chat-box').show();
+
+    $('#username').val("");
+})
+
+
